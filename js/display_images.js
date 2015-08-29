598c7ade8f25372388d646483ebd78ce78e84072
Julia Van Cleve <julia@jmv.me>
2015-08-29 14:16:41 -0400

    added gists to github landing page

--- js/display_images.js
+++ js/display_images.js
@@ -6,12 +6,20 @@ $(document).ready(function() {
   })
     .done(function(result) {
       for (i in result) {
-          photo_title = result[i].title
-          photo_location = "http://localhost:3000" + result[i].image.image.resized.url
-          thumb_location = "http://localhost:3000" + result[i].image.image.small_thumb.url
-          row_order_num = result[i].row_order
-          console.log(row_order_num)
-          $("div#content").append(" <a href=' " + photo_location + " ' data-lightbox='gallery' data-title=' " + photo_title + " '><img src=' " + thumb_location + "' />")
+        album_title = result[i].album.title;
+        // Do not display "hidden" categories
+        if (album_title != "Uncategorized" && album_title != "uncategorized" && album_title != "null") {
+          photo_title = result[i].title;
+          photo_location = "http://localhost:3000" + result[i].image.image.resized.url;
+          thumb_location = "http://localhost:3000" + result[i].image.image.small_thumb.url;
+          var album_title_formatted = album_title.toLowerCase().split(' ').join('_');
+          if (document.getElementById(album_title_formatted) == undefined) {
+            var new_album = $("<div>", {id: album_title_formatted, class: "album"});
+            $("div#content").append("<h1 class='album'>"+album_title+"</h1>").append(new_album);
+          }
+          var album_div_name = "div#" + album_title_formatted;
+          $(album_div_name).append(" <a href=' " + photo_location + " ' data-lightbox='gallery' data-title=' " + photo_title + " '><img src=' " + thumb_location + "' />");
+        }
       }
     })
 });
